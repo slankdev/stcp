e5ed309c9ee56209f8e8d4ae5c0de66b0bfbb3fd
slank-arch <slank.dev@gmail.com>
2016-07-02 15:13:47 +0900

    [ADD] rte's basic functions

--- src/dataplane/dpdk.h
+++ src/dataplane/dpdk.h
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <inttypes.h>
+#include <errno.h>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <exception>
 
 #include <rte_config.h>
 #include <rte_version.h>
@@ -15,124 +22,101 @@
 #include <rte_hexdump.h>
 
 
-class dpdk {
-    private:
-
-    public:
-        struct rte_mempool* mbuf_pool;
-
-
-
-        void eth_dev_init(int argc, char** argv);
-        void eth_dev_configure(uint8_t port_id, uint16_t nb_rx_queue,
-                uint16_t nb_tx_queue, const struct rte_eth_conf* eth_conf);
-        void eth_rx_queue_setup(uint8_t port_id, 
-                uint16_t rx_queue_id, 
-                uint16_t nb_rx_desc, uint32_t socket_id, 
-                const struct rte_eth_rxconf* rx_conf);
-        void eth_tx_queue_setup(uint8_t port_id, 
-                uint16_t tx_queue_id, 
-                uint16_t nb_tx_desc, uint32_t socket_id, 
-                const struct rte_eth_txconf* tx_conf);
-        void eth_dev_start(uint8_t);
-        void eth_promiscuous_enable(uint8_t);
-        void pktmbuf_pool_create(const char* name, uint32_t n, 
-                uint32_t cache_size, uint16_t priv_size, 
-                uint16_t data_room_size, int socket_id);
-        size_t eth_dev_count();
-        size_t lcore_count();
-        
-        void eth_macaddr_get(uint8_t port, struct ether_addr* addr);
-        uint32_t socket_id();
-
-    public:
-        dpdk();
-        ~dpdk();
-
-        void port_init();
-};
-
 
 
 namespace rte {
 
 
-
-/* This class provides config of ether_port */
-#include <rte_ethdev.h>
-
-
-
-class eth_rxmode {
+class exception : public std::exception {
+    private:
+        std::string str;
     public:
-        enum rte_eth_rx_mq_mode mq_mode;
-        uint32_t max_rx_pkt_len;  
-        uint16_t split_hdr_size;  
-        uint16_t header_split : 1, 
-                 hw_ip_checksum   : 1, 
-                 hw_vlan_filter   : 1, 
-                 hw_vlan_strip    : 1, 
-                 hw_vlan_extend   : 1, 
-                 jumbo_frame      : 1, 
-                 hw_strip_crc     : 1, 
-                 enable_scatter   : 1, 
-                 enable_lro       : 1; 
-    public:
-        eth_rxmode() : 
-            mq_mode((rte_eth_rx_mq_mode)0),
-            max_rx_pkt_len(ETHER_MAX_LEN), 
-            split_hdr_size(0) {}
-        
-};
+        explicit exception(const char* s="") noexcept {
+            int e = errno;
+            str = s; 
+            if (e != 0)
+                str += strerror(e);
+        } 
 
-
-class eth_txmode {
-    public:
-        enum rte_eth_tx_mq_mode mq_mode; 
-        uint16_t pvid;
-        uint8_t hw_vlan_reject_tagged : 1,
-                hw_vlan_reject_untagged : 1,
-                hw_vlan_insert_pvid : 1;
-    public:
-        eth_txmode() :
-            mq_mode((rte_eth_tx_mq_mode)0),
-            pvid(0) {}
-};
-
-class eth_conf {
-    public:
-        uint32_t link_speeds; 
-        rte::eth_rxmode rxmode; 
-        struct rte_eth_txmode txmode; 
-        uint32_t lpbk_mode; 
-        struct {
-            struct rte_eth_rss_conf rss_conf; 
-            struct rte_eth_vmdq_dcb_conf vmdq_dcb_conf;
-            struct rte_eth_dcb_rx_conf dcb_rx_conf;
-            struct rte_eth_vmdq_rx_conf vmdq_rx_conf;
-        } rx_adv_conf; 
-        union {
-            struct rte_eth_vmdq_dcb_tx_conf vmdq_dcb_tx_conf;
-            struct rte_eth_dcb_tx_conf dcb_tx_conf;
-            struct rte_eth_vmdq_tx_conf vmdq_tx_conf;
-        } tx_adv_conf; 
-        uint32_t dcb_capability_en;
-        struct rte_fdir_conf fdir_conf; 
-        struct rte_intr_conf intr_conf; 
-
-    public:
-        eth_conf() : link_speeds(0), lpbk_mode(0), dcb_capability_en(0) 
-        {
-            memset(&rx_adv_conf, 0, sizeof rx_adv_conf);
-            memset(&tx_adv_conf, 0, sizeof rx_adv_conf);
-            memset(&fdir_conf, 0, sizeof fdir_conf);
-            memset(&intr_conf, 0, sizeof intr_conf);
+        template<class T>
+        exception& operator<<(const T& t) noexcept {
+            std::ostringstream os;
+            os << " " << t ;
+            str += os.str();
+            return *this;
+        }
+        const char* what() const noexcept {
+            return str.c_str(); 
         }
 };
 
 
 
 
-} /* namespace */
 
+void eth_dev_init(int argc, char** argv);
+void eth_dev_configure(uint8_t port_id, uint16_t nb_rx_queue,
+        uint16_t nb_tx_queue, const struct rte_eth_conf* eth_conf);
+void eth_rx_queue_setup(uint8_t port_id, 
+        uint16_t rx_queue_id, 
+        uint16_t nb_rx_desc, uint32_t socket_id, 
+        const struct rte_eth_rxconf* rx_conf,
+        struct rte_mempool* mbuf_pool);
+void eth_tx_queue_setup(uint8_t port_id, 
+        uint16_t tx_queue_id, 
+        uint16_t nb_tx_desc, uint32_t socket_id, 
+        const struct rte_eth_txconf* tx_conf);
+void eth_dev_start(uint8_t);
+void eth_promiscuous_enable(uint8_t port_id);
+void eth_promiscuous_disable(uint8_t port_id);
+int  eth_promiscuous_get(uint8_t port_id);
+struct rte_mempool* pktmbuf_pool_create(const char* name, 
+        uint32_t n, uint32_t cache_size, uint16_t priv_size, 
+        uint16_t data_room_size, int socket_id);
+void eth_macaddr_get(uint8_t port, struct ether_addr* addr);
+size_t eth_dev_count();
+size_t lcore_count();
+uint16_t eth_rx_burst(uint8_t port_id, uint16_t queue_id, 
+        struct rte_mbuf** rx_pkts, const uint16_t nb_pkts);
+uint16_t eth_tx_burst(uint8_t port_id, uint16_t queue_id, 
+        struct rte_mbuf** tx_pkts, uint16_t nb_pkts);
+void pktmbuf_free(struct rte_mbuf* m);
+uint32_t socket_id();
+void pktmbuf_dump(FILE* f,const struct rte_mbuf* m, unsigned dump_len);	
+int eth_dev_socket_id(uint8_t port_id);
+
+
+
+
+
+} /* namespace rte */
+
+
+
+
+
+
+
+
+#include <rte_ethdev.h>
+class dpdk_conf {
+public:
+    struct rte_eth_conf port_conf;
+
+    dpdk_conf()
+    {
+        init_port_conf();
+    }
+    ~dpdk_conf() {}
+
+    void init_port_conf()
+    {
+        struct rte_eth_rxmode rxmode;
+        memset(&rxmode, 0, sizeof rxmode);
+        rxmode.max_rx_pkt_len = ETHER_MAX_LEN;
+
+        memset(&port_conf, 0, sizeof(port_conf));
+        port_conf.rxmode = rxmode;
+    }
+};
 
--- src/dataplane/main.cc
+++ src/dataplane/main.cc
@@ -1,5 +1,4 @@
 
-
 #include "dpdk.h"
 
 #define RX_RING_SIZE 128
@@ -9,92 +8,74 @@
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE      32
 
+dpdk_conf dpdk_conf;
 
-static void lcore_main(dpdk& d)
+
+
+static void lcore_main()
 {
-    const uint8_t num_ports = d.eth_dev_count();
+    const uint8_t num_ports = rte::eth_dev_count();
 
     for (uint8_t port = 0; port < num_ports; port++) {
-        if (rte_eth_dev_socket_id(port) > 0 && rte_eth_dev_socket_id(port) != (int)rte_socket_id())
+        if (rte::eth_dev_socket_id(port) > 0 && 
+                rte::eth_dev_socket_id(port) != (int)rte::socket_id()) {
             printf("WARNING: port %u is on remote NUMA node to "
                     "polling thread. \n\tPerformance will "
                     "not be optimal. \n ", port);
+        }
     }
-    printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n", rte_lcore_id());
-
 
     for (;;) {
-        
         for (uint8_t port=0; port<num_ports; port++) {
             struct rte_mbuf* bufs[BURST_SIZE];
-            const uint16_t num_rx = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
+            const uint16_t num_rx = rte::eth_rx_burst(port, 0, bufs, BURST_SIZE);
 
-            if (unlikely(num_rx == 0))
-                continue;
+            if (unlikely(num_rx == 0)) continue;
 
             for (uint16_t i=0; i<num_rx; i++) {
-                rte_pktmbuf_dump(stdout, bufs[i], sizeof(struct rte_mbuf));
-                /* rte_hexdump(stdout, "recv packet", */
-                /*         rte_pktmbuf_mtod(bufs[i], void*) , */
-                /*         rte_pktmbuf_data_len(bufs[i])     ); */
+                rte::pktmbuf_dump(stdout, bufs[i], sizeof(struct rte_mbuf));
                 uint8_t* head = rte_pktmbuf_mtod(bufs[i], uint8_t*);
                 memset(head , 0xee, 6);
             }
 
+            const uint16_t num_tx = rte::eth_tx_burst(port, 0, bufs, num_rx);
+            printf("Reflect %d packet !! \n", num_rx);
 
-            const uint16_t num_tx = 0;
-            /* const uint16_t num_tx = rte_eth_tx_burst(port, 0, bufs, num_rx); */
-            /* printf("Reflect %d packet !! \n", num_rx); */
             if (unlikely(num_tx < num_rx)) {
                 uint16_t buf;
                 for (buf=num_tx; buf<num_rx; buf++)
-                    rte_pktmbuf_free(bufs[buf]);
+                    rte::pktmbuf_free(bufs[buf]);
             }
-
             printf("\n\n");
-
         }
     }
 }
 
 
 
-static int port_init(dpdk& d, uint8_t port)
+static int port_init(struct rte_mempool* mempool, uint8_t port)
 {
-#if 0
-    const struct rte_eth_conf port_conf_default = {
-            .rxmode = { .max_rx_pkt_len = ETHER_MAX_LEN }
-    };
-#else
-    struct rte_eth_rxmode rxmode;
-    memset(&rxmode, 0, sizeof rxmode);
-    rxmode.max_rx_pkt_len = ETHER_MAX_LEN;
-    struct rte_eth_conf port_conf_default;
-    memset(&port_conf_default, 0, sizeof(port_conf_default));
-    port_conf_default.rxmode = rxmode;
-#endif
 
-    struct rte_eth_conf port_conf = port_conf_default;
-
-    if (port >= d.eth_dev_count()) return -1;
+    struct rte_eth_conf port_conf = dpdk_conf.port_conf;
+    if (port >= rte::eth_dev_count()) return -1;
 
     const uint16_t rx_rings = 1;
     const uint16_t tx_rings = 1;
-    d.eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
+    rte::eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
 
 
     for (uint16_t q=0; q < rx_rings; q++)
-        d.eth_rx_queue_setup(port, q, RX_RING_SIZE, 
-                rte_eth_dev_socket_id(port), NULL);
+        rte::eth_rx_queue_setup(port, q, RX_RING_SIZE, 
+                rte::eth_dev_socket_id(port), NULL, mempool);
 
     for (uint16_t q=0; q < tx_rings; q++)
-        d.eth_tx_queue_setup(port, q, TX_RING_SIZE, 
-                rte_eth_dev_socket_id(port), NULL);
+        rte::eth_tx_queue_setup(port, q, TX_RING_SIZE, 
+                rte::eth_dev_socket_id(port), NULL);
 
-    d.eth_dev_start(port);
+    rte::eth_dev_start(port);
 
     struct ether_addr addr;
-    d.eth_macaddr_get(port, &addr);
+    rte::eth_macaddr_get(port, &addr);
     printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
             " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
             (unsigned)port,
@@ -102,34 +83,29 @@ static int port_init(dpdk& d, uint8_t port)
             addr.addr_bytes[2], addr.addr_bytes[2],
             addr.addr_bytes[3], addr.addr_bytes[4]);
 
-	d.eth_promiscuous_enable(port);
-
+    rte::eth_promiscuous_enable(port);
     return 0;
 }
 
 
 
-
 int main(int argc, char** argv)
 {
-    dpdk d;
-    d.eth_dev_init(argc, argv);
-    
-    uint8_t num_ports = d.eth_dev_count();
-    printf("%d ports found  \n", num_ports);
+    rte::eth_dev_init(argc, argv);
+    uint8_t num_ports = rte::eth_dev_count();
+
     if (num_ports < 1)
         rte_exit(EXIT_FAILURE, "eth_dev_count()");
 
-    d.pktmbuf_pool_create(
+    struct rte_mempool* mempool = rte::pktmbuf_pool_create(
             "SLANK", NUM_MBUFS*num_ports, 
             MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, 
-            rte_socket_id());
+            rte::socket_id());
 
     for (uint8_t portid=0; portid < num_ports; portid++) {
-        if (port_init(d, portid) != 0)
+        if (port_init(mempool, portid) != 0)
             rte_exit(EXIT_FAILURE, "Can't init port%u\n", portid);
     }
-    // ここまで完了した。
-
-    lcore_main(d);
+    lcore_main();
 }
+
