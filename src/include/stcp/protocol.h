c0b5e2730337cf05e2946b31efd40a4d52298aaa
slank@cybozu <slank.dev@gmail.com>
2016-08-03 13:35:15 +0900

    [IMPL] arp_module

--- src/include/stcp/protocol.h
+++ src/include/stcp/protocol.h
@@ -48,20 +48,6 @@ public:
 };
 
 
-class arp_module {
-private:
-    proto_module m;
-public:
-    arp_module() { m.name = "ARP"; }
-    void init() {m.init();}
-    void rx_push(struct rte_mbuf* msg){m.rx_push(msg);}
-    void tx_push(struct rte_mbuf* msg){m.tx_push(msg);}
-    struct rte_mbuf* rx_pop() {return m.rx_pop();}
-    struct rte_mbuf* tx_pop() {return m.tx_pop();}
-    void drop(struct rte_mbuf* msg) {m.drop(msg);}
-    void proc() {m.proc();}
-    void stat() {m.stat();}
-};
 
 
 class ip_module {
