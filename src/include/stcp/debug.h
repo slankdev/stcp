92c73a5c3e95540965d5497733a837e989e759ff
slank@mbp <slank.dev@gmail.com>
2016-12-14 12:07:14 +0900

    [REFACT] about runlevel

--- src/include/stcp/debug.h
+++ src/include/stcp/debug.h
@@ -6,14 +6,28 @@
 namespace stcp {
 
 
-#define DPRINT(...) \
+
+#if ST_RUNLEVEL==RUNLEV_SPEED
+
+# define DPRINT(...) printf(...)
+# define stcp_printf(...)
+
+#elif ST_RUNLEVEL==RUNLEV_DEBUG
+
+# define DPRINT(...) \
     core::stcp_stddbg.fprintf("%-15s:%4d: ", __FILE__, __LINE__); \
     core::stcp_stddbg.fprintf(__VA_ARGS__); \
     core::stcp_stddbg.fflush();
-
-#define stcp_printf(...) \
+# define stcp_printf(...) \
     core::stcp_stdout.fprintf(__VA_ARGS__); \
     core::stcp_stdout.fflush();
+
+#else
+# error "unknown runlevel"
+#endif
+
+
+
 
 
 } /* namespace stcp */
