e395d3344c62bf559f5408745f04fab9dc69a811
slank@mbp <slank.dev@gmail.com>
2017-01-31 11:47:24 +0900

    [Work]

--- lib/include/ssnlib_shell.h
+++ lib/include/ssnlib_shell.h
@@ -24,20 +24,6 @@ static inline char* Readline(const char* p)
 namespace ssnlib {
 
 class Shell : public ssnlib::ssn_thread {
-    class Cmd_help : public Command {
-        Shell* shell;
-    public:
-        Cmd_help(Shell* s) : shell(s) { name = "help"; }
-        void operator()(const std::vector<std::string>& args)
-        {
-            UNUSED(args);
-            printf("Commands: \n");
-            for (Command* c : shell->cmds) {
-                printf("  %s \n", c->name.c_str());
-            }
-        }
-    };
-
     std::vector<ssnlib::Command*> cmds;
     System* sys;
 public:
@@ -48,15 +34,23 @@ public:
         add_cmd(new Cmd_quit   (sys));
         add_cmd(new Cmd_thread (sys));
         add_cmd(new Cmd_show   (sys));
-        add_cmd(new Cmd_help   (this));
     }
 
-    ~Shell() { for (ssnlib::Command* cmd : cmds) delete(cmd); }
+    ~Shell() { for (ssnlib::Command* cmd : cmds) delete(cmd); } // TODO
+
+    void help()
+    {
+        printf("Commands: \n");
+        for (const Command* c : cmds) {
+            printf("  %s \n", c->name.c_str());
+        }
+    }
 
     bool kill() { return false; }
 
     void add_cmd(ssnlib::Command* newcmd)
     {
+        printf("SHELL: add command [%s]\n", newcmd->name.c_str());
         cmds.push_back(newcmd);
     }
 
@@ -65,13 +59,17 @@ public:
     {
         if (strlen(cmd_str) == 0) return;
         std::vector<std::string> args = slankdev::split(cmd_str, ' ');
-        for (ssnlib::Command* cmd : cmds) {
-            if (cmd->name == args[0]) {
-                (*cmd)(args);
-                return;
+        if (args[0] == "help") {
+            help();
+        } else {
+            for (ssnlib::Command* cmd : cmds) {
+                if (cmd->name == args[0]) {
+                    (*cmd)(args);
+                    return;
+                }
             }
+            printf("SUSH: command not found: %s\n", args[0].c_str());
         }
-        printf("SUSH: command not found: %s\n", args[0].c_str());
     }
 
 
